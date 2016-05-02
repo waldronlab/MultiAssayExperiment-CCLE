7e9966450832f50464f40df9fe632c54bdfb4bc0
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-05-02 13:00:09 -0400

    minor changes / comments
    minor changes and comments, prior to cleanup

--- R/CCLEMAEO.R
+++ R/CCLEMAEO.R
@@ -1,11 +1,29 @@
+# load readr
 library(readr)
-library(GSRI) # for readGct function
-pdata <- read_csv("rawdata/CCLE_NP24.2009_Drug_data_2012.02.20.csv")
-myMAF <- read_delim("rawdata/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf", delim = "\t", na = "<NA>")
-newEx <- read_delim("rawdata/CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t")
-# NewMatrix <- readGct("rawdata/CCLE_Expression_Entrez_2012-09-29.gct")
-fastMat <- read_delim("rawdata/CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t", skip = 2)
+#library(CePa)
+#library(GSRI) # for readGct function
+
+# orininal working directory
+working0 <- getwd()
+
+# data directory
+data <- "~/Source/MultiAssayExperiment-CCLE/data"
+
+# set working directory as data directory
+setwd(data)
+
+# read in data sets
+pdata <- read_csv("CCLE_NP24.2009_Drug_data_2012.02.20.csv")
+myMAF <- read_delim("CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf", delim = "\t", na = "<NA>")
+newEx <- read_delim("CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t")
+#NewMatrix <- readGct("CCLE_Expression_Entrez_2012-09-29.gct")
+#testM <- read.gct("CCLE_Expression_Entrez_2012-09-29.gct")
+fastMat <- read_delim("CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t", skip = 2)
+copyNum <- read_delim("CCLE_copynumber_byGene_2012-09-29.txt", delim = "\t")
+
+# add rownames to fastMat
 rownames(fastMat) <- fastMat$Name
 fastMat <- fastMat[, -which(names(fastMat) == "Name")]
 
-copyNum <- read_delim("rawdata/CCLE_copynumber_byGene_2012-09-29.txt", delim = "\t")
+# restore orininal working directory
+setwd(working0)
