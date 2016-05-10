e3128352996108c71452310043a166220156988c
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-05-10 12:45:57 -0400

    skeleton of constructor

--- R/ccleMAEO.R
+++ R/ccleMAEO.R
@@ -10,34 +10,50 @@
 # load necessary packages
 library(readr)
 library(MultiAssayExperiment)
-
-# data directories
-data <- "rawdata"
-
-# set working directory as data directory
-setwd(data)
+library(TCGAmisc)
 
 # read in data sets
-DNAcopyNumber <- read_delim("CCLE_copynumber_byGene_2012-09-29.txt", delim = "\t")
-mRNAexpression <-read_delim("CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t", skip = 2)
-mutations <- read_delim("CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf", delim = "\t", na = "<NA>")
-pData <- read_csv("CCLE_NP24.2009_Drug_data_2012.02.20.csv")
+DNAcopyNumber <- read_delim("rawdata/CCLE_copynumber_byGene_2012-09-29.txt", delim = "\t")
+mRNAexpression <-read_delim("rawdata/CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t", skip = 2)
+mutations <- read_delim("rawdata/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf", delim = "\t", na = "<NA>")
+pData <- read_csv("rawdata/CCLE_NP24.2009_Drug_data_2012.02.20.csv")
 
 # add rownames to mRNAexpression
 rownames(mRNAexpression) <- mRNAexpression$Name
 mRNAexpression <- mRNAexpression[, -which(names(mRNAexpression) == "Name")]
 
 # save objects as rsd files
-saveRDS(DNAcopyNumber, file = "DNAcopyNumber.rds")
-saveRDS(mRNAexpression, file = "mRNAexpression.rds")
-saveRDS(mutations, file = "mutations.rds")
-saveRDS(pData, file = "pData.rds")
+saveRDS(DNAcopyNumber, file = "rdsdata/DNAcopyNumber.rds")
+saveRDS(mRNAexpression, file = "rdsdata/mRNAexpression.rds")
+saveRDS(mutations, file = "rdsdata/mutations.rds")
+saveRDS(pData, file = "rdsdata/pData.rds")
 
 # load objects from rsd files
-DNAcopyNumber <- readRDS("DNAcopyNumber.rds")
-mRNAexpression <- readRDS("mRNAexpression.rds")
-mutations <- readRDS("mutations.rds")
-pData <- readRDS("pData.rds")
+DNAcopyNumber <- readRDS("rdsdata/DNAcopyNumber.rds")
+mRNAexpression <- readRDS("rdsdata/mRNAexpression.rds")
+mutations <- readRDS("rdsdata/mutations.rds")
+pData <- readRDS("rdsdata/pData.rds")
 
-# generate CCLE MAEO
-#TODO
+# pData specimine ID function, everything after underscore
+# use IDs as the rownames of the pData
+rownames(pData) <- paste(pData$`CCLE Cell Line Name`, pData$Compound, pData$Target, sep = ".")
+
+
+# create a ExpressionSet or RangeSummarizedExperiment from
+# DNAcopyNumber, available from SummarizedExperiment package
+?ExpressionSet()
+?SummarizedExperiment()
+
+# create a ExpressionSet from mRNAexpression
+?ExpressionSet()
+
+# create a GRangesList from mutations
+?GRangesList()
+
+# split the tummor sample barcode and make list thereof
+# duplicate makeGRangesList method from TCGAmics package
+?makeGRangesList()
+
+# generate CCLE MAEO by passing these arguments to the MultiAssayExperiment method
+# specimine ID (short spec), assay (long spec), assayname (e.g. DNAcopyNumber)
+?MultiAssayExperiment()
