084b73350a9da11a745e4d32fd4dafb46ac8f8dd
LiNk-NY <mramos09@gmail.com>
2016-05-11 16:18:00 -0400

    add function to create RSE from data.frame

--- R/ccleMAEO.R
+++ R/ccleMAEO.R
@@ -11,8 +11,11 @@
 library(readr)
 library(MultiAssayExperiment)
 library(TCGAmisc)
+library(SummarizedExperiment)
+library(GenomicRanges)
 
 # read in data sets
+if (!dir.exists("rawdata")) {dir.create("rawdata")}
 DNAcopyNumber <- read_delim("rawdata/CCLE_copynumber_byGene_2012-09-29.txt", delim = "\t")
 mRNAexpression <-read_delim("rawdata/CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t", skip = 2)
 mutations <- read_delim("rawdata/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf", delim = "\t", na = "<NA>")
@@ -23,6 +26,7 @@ rownames(mRNAexpression) <- mRNAexpression$Name
 mRNAexpression <- mRNAexpression[, -which(names(mRNAexpression) == "Name")]
 
 # save objects as rsd files
+if (!dir.exists("rdsdata")) {dir.create("rdsdata")}
 saveRDS(DNAcopyNumber, file = "rdsdata/DNAcopyNumber.rds")
 saveRDS(mRNAexpression, file = "rdsdata/mRNAexpression.rds")
 saveRDS(mutations, file = "rdsdata/mutations.rds")
@@ -34,15 +38,15 @@ mRNAexpression <- readRDS("rdsdata/mRNAexpression.rds")
 mutations <- readRDS("rdsdata/mutations.rds")
 pData <- readRDS("rdsdata/pData.rds")
 
-# pData specimine ID function, everything after underscore
+# pData specimen ID function, everything after underscore
 # use IDs as the rownames of the pData
 rownames(pData) <- paste(pData$`CCLE Cell Line Name`, pData$Compound, pData$Target, sep = ".")
 
 
 # create a ExpressionSet or RangeSummarizedExperiment from
 # DNAcopyNumber, available from SummarizedExperiment package
-?ExpressionSet()
-?SummarizedExperiment()
+source("R/makeRSE.R")
+newRSE <- makeRSE(DNAcopyNumber)
 
 # create a ExpressionSet from mRNAexpression
 ?ExpressionSet()
---
+++ R/makeRSE.R
@@ -0,0 +1,27 @@
+makeRSE <- function(dataset){
+    grangeNames <- grep("name|chr|start|end$", names(dataset),
+                        value = TRUE, ignore.case = TRUE)
+    if (length(grangeNames) != 4L) {warning("not all rowRange columns found")}
+    DFranges <- DataFrame(dataset[, grangeNames])
+    chrname <- grep("chr", names(DFranges), value = TRUE, ignore.case = TRUE)
+    genName <- grep("name", names(DFranges), value = TRUE, ignore.case = TRUE)
+    if (all(grepl("^[0-9]+$", sample(DFranges[, chrname], size = 5)))) {
+        DFranges[, chrname] <- paste0("chr", DFranges[, chrname])
+    }
+    if (!chrname %in% c("seqnames", "chr", "chrom")) {
+        names(DFranges)[which(names(DFranges) == chrname)] <- "chrom"
+    }
+    RowRanges <- as(DFranges[,!grepl("name", grangeNames, ignore.case = TRUE)],
+                    "GRanges")
+    if(length(unique(DFranges[, genName])) == dim(DFranges)[1]) {
+        names(RowRanges) <- DFranges[, genName]
+        rNames <- DFranges[, genName]
+    }
+    dm <- dataset[, !(names(dataset) %in% grangeNames)]
+    if(exists("rNames")) {
+        rownames(dm) <- rNames
+    }
+    newSE <- SummarizedExperiment::SummarizedExperiment(
+        assays = SimpleList(counts = dm), rowRanges = RowRanges)
+    return(newSE)
+}
