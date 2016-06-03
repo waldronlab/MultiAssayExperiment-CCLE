d1307522a94354d5352015fe2437858f968bbefb
LiNk-NY <mramos09@gmail.com>
2016-06-03 18:05:28 -0400

    create ccleMAEO

--- R/ccleMAEO.R
+++ R/ccleMAEO.R
@@ -10,7 +10,7 @@
 # load necessary packages
 library(readr)
 library(S4Vectors)
-library(TCGAmisc)
+library(BiocInterfaces)
 library(Biobase)
 library(SummarizedExperiment)
 library(GenomicRanges)
@@ -19,7 +19,7 @@ library(MultiAssayExperiment)
 # read in data sets
 if (!dir.exists("rawdata")) {dir.create("rawdata")}
 DNAcopyNumber <- read_delim("rawdata/CCLE_copynumber_byGene_2012-09-29.txt", delim = "\t")
-mRNAexpression <-read_delim("rawdata/CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t", skip = 2)
+mRNAexpression <- read_delim("rawdata/CCLE_Expression_Entrez_2012-09-29.gct", delim = "\t", skip = 2)
 mutations <- read_delim("rawdata/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf", delim = "\t", na = "<NA>")
 pData <- read_csv("rawdata/CCLE_NP24.2009_Drug_data_2012.02.20.csv")
 pData <- DataFrame(pData)
@@ -29,6 +29,7 @@ pData <- drugDataFrame(splitData, c("Doses..uM.", "Activity.Data..median.", "Act
 pData$TissueOrigin <- gsub("^[^_]+_", "", rownames(pData), perl = TRUE)
 
 # add rownames to mRNAexpression
+mRNAexpression <- DataFrame(mRNAexpression)
 rownames(mRNAexpression) <- mRNAexpression$Name
 mRNAexpression <- mRNAexpression[, -which(names(mRNAexpression) == "Name")]
 annoteFeatures <- mRNAexpression[, "Description"]
@@ -62,15 +63,31 @@ rownames(pData)
 
 # create a ExpressionSet or RangeSummarizedExperiment from
 # DNAcopyNumber, available from SummarizedExperiment package
-source("R/makeRSE.R")
-newRSE <- makeRSE(DNAcopyNumber)
+newRSE <- makeRSE(DataFrame(DNAcopyNumber), regEx = c("chr$", "start$", "end$", "strand"))
 
 # create a GRangesList from mutations
 newMut <- makeGRangesList(as.data.frame(mutations, stringsAsFactors = FALSE),
                                         ccle(primary = "Tumor_Sample_Barcode",
                                         standard = TRUE, idFUN = I))
+newMut <- RangedRaggedAssay(newMut)
 
 
 # generate CCLE MAEO by passing these arguments to the MultiAssayExperiment method
 # specimine ID (short spec), assay (long spec), assayname (e.g. DNAcopyNumber)
 ?MultiAssayExperiment()
+
+dataList <- list(CNA = newRSE, Mutations = newMut, mRNA = mRNAEset)
+## Insert map step here
+
+## Convenient function for removing the first part of the CCLE ID:
+getCellID <- function(fullCCLE) {
+    stopifnot(is.character(fullCCLE))
+    cellID <- gsub("(^[^_]+)_\\w+", "\\1", fullCCLE, perl = TRUE)
+    return(cellID)
+}
+
+CCMap <- generateMap(dataList, pData)
+newList <- PrepMultiAssay(dataList, pData, CCMap)
+
+ccleMAEO <- MultiAssayExperiment(newList$Elist, newList$pData, newList$sampleMap)
+
