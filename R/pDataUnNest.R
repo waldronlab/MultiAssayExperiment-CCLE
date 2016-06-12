7b3f10be170e444bea722f39d9912fd620138cc0
LiNk-NY <mramos09@gmail.com>
2016-06-12 17:28:08 -0400

    Find combinations of compounds and targets

--- R/pDataUnNest.R
+++ R/pDataUnNest.R
@@ -15,3 +15,28 @@ pData <- S4Vectors::split(pData, CellLineName)
 class(pData)
 
 pData
+names(pData[[1]])
+
+combos <- Reduce(union, lapply(pData, function(CellLine) {
+    apply(CellLine, 1, function(dataset) {
+        paste(dataset["Compound"], dataset["Target"], sep = ":")
+    })
+}))
+
+## Test all unique despite case
+length(unique(toupper(combos))) == length(combos)
+
+## Not working
+getDrugMatrix <- function(dataList, drugCol) {
+extractedVectors <- lapply(dataList,
+                           function(DF, columnRequest) {
+                               DF[, columnRequest]
+                               }, columnRequest = drugCol)
+fullVector <- Reduce(union, extractedVectors)
+FullList <- lapply(extractedVectors,
+                   function(CellLine, AllIn)
+                       { AllIn[match(CellLine, AllIn)]}, AllIn = fullVector)
+drugList <- do.call(cbind, FullList)
+drugMatrix <- as.matrix(drugList)
+return(drugMatrix)
+}
