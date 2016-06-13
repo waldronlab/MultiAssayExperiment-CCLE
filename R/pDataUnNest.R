3eab06922e9fb2b5434ceffae9277ef10cbbaaa6
LiNk-NY <mramos09@gmail.com>
2016-06-12 23:53:49 -0400

    Annotate and create matrix for select columns

--- R/pDataUnNest.R
+++ R/pDataUnNest.R
@@ -17,26 +17,38 @@ class(pData)
 pData
 names(pData[[1]])
 
-combos <- Reduce(union, lapply(pData, function(CellLine) {
+# Create a list of concatenated compound and target name combinations
+concatCol <- lapply(pData, function(CellLine) {
     apply(CellLine, 1, function(dataset) {
         paste(dataset["Compound"], dataset["Target"], sep = ":")
     })
-}))
+})
 
-## Test all unique despite case
+numIndex <- seq_along(pData)
+names(numIndex) <- names(pData)
+
+# Add unique compound:target vector as pData column
+pData2 <- lapply(numIndex, function(i, CellLine, comboTarget) {
+    cbind(CellLine[[i]], DataFrame(CompTarget = comboTarget[[i]]))
+}, CellLine = pData, comboTarget = concatCol)
+
+# Get all possible drug-target combinations
+combos <- Reduce(union, concatCol)
+
+## Test that all are unique regardless of case
 length(unique(toupper(combos))) == length(combos)
 
-## Not working
-getDrugMatrix <- function(dataList, drugCol) {
-extractedVectors <- lapply(dataList,
-                           function(DF, columnRequest) {
-                               DF[, columnRequest]
-                               }, columnRequest = drugCol)
-fullVector <- Reduce(union, extractedVectors)
-FullList <- lapply(extractedVectors,
-                   function(CellLine, AllIn)
-                       { AllIn[match(CellLine, AllIn)]}, AllIn = fullVector)
-drugList <- do.call(cbind, FullList)
-drugMatrix <- as.matrix(drugList)
+## Create a matrix by extracting one column from pData for all CellLines and
+## drug-target combinations
+getDrugMatrix <- function(dataList, targetColumn, features) {
+FullList <- lapply(dataList, function(DF) {
+    DF[, targetColumn][match(features, DF[, "CompTarget"])]
+})
+drugMatrix <- do.call(cbind, FullList)
+rownames(drugMatrix) <- features
+colnames(drugMatrix) <- names(dataList)
 return(drugMatrix)
 }
+
+names(pData2[[1]])
+getDrugMatrix(pData2, "EC50.uM.", combos)
